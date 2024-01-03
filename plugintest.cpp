3742b17a7681a5d5594a692b696bb6113e4a4ca1
Abhilash R <abhilash.kollam@gmail.com>
2024-01-03 11:48:53 +0530

    adding plugin changes

--- plugintest.cpp
+++ plugintest.cpp
@@ -1,27 +1,36 @@
-#include "http/app.hpp"
 #include "plugin-ifaces/router-plugin.h"
-#include "shared_library.hpp"
-#include <boost/asio.hpp>
-std::shared_ptr<boost::asio::io_context> getIoContext() {
-  static std::shared_ptr<boost::asio::io_context> io =
-      std::make_shared<boost::asio::io_context>();
-  return io;
-}
-int main() {
-  try {
-    std::shared_ptr<boost::asio::io_context> io = getIoContext();
-    boost::asio::any_io_executor executor = io->get_executor();
-    crow::App app(io);
-    bmcweb::PluginDb db("/tmp");
-    auto interfaces = db.getInterFaces<bmcweb::RouterPlugin>();
-    for (auto &plugin : interfaces) {
-      BMCWEB_LOG_INFO("Registering plugin ");
-      BMCWEB_LOG_INFO("{}", plugin->registerRoutes(app, executor));
-    }
-    io->run();
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << '\n';
-  }
 
-  return 0;
+#include "http/app.hpp"
+#include "shared_library.hpp"
+
+#include <boost/asio.hpp>
+std::shared_ptr<boost::asio::io_context> getIoContext()
+{
+    static std::shared_ptr<boost::asio::io_context> io =
+        std::make_shared<boost::asio::io_context>();
+    return io;
+}
+int main()
+{
+    try
+    {
+        std::shared_ptr<boost::asio::io_context> io = getIoContext();
+        boost::asio::any_io_executor executor = io->get_executor();
+        crow::App app(io);
+        bmcweb::PluginDb db(
+            "/Users/abhilashraju/work/cpp/crow_core/build/redfish_event");
+        auto interfaces = db.getInterFaces<bmcweb::RouterPlugin>();
+        for (auto& plugin : interfaces)
+        {
+            BMCWEB_LOG_INFO("Registering plugin ");
+            BMCWEB_LOG_INFO("{}", plugin->registerRoutes(app));
+        }
+        io->run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
 }
--- redfish_event/src/redfish_event.cpp
+++ redfish_event/src/redfish_event.cpp
@@ -1,74 +1,85 @@
-#include "event_manager/event_manager.hpp"
 #include "router-plugin.h"
+
+#include "event_manager/event_manager.hpp"
+
 #include <app.hpp>
 #include <boost/dll/alias.hpp> // for BOOST_DLL_ALIAS
 #include <nlohmann/json.hpp>
 
 #include <iostream>
 #include <memory>
-namespace bmcweb {
+namespace bmcweb
+{
 
-class MyRouterPlugin : public RouterPlugin {
-public:
-  MyRouterPlugin() {}
-  std::string registerRoutes(crow::App &app,
-                             boost::asio::any_io_executor &executor) {
-
-    sendTestEventLog(app, executor);
-    return "MyRouterPlugin";
-  }
-
-  // Factory method
-  static std::shared_ptr<MyRouterPlugin> create() {
-    return std::make_shared<MyRouterPlugin>();
-  }
-
-  bool hasInterface(const std::string &interfaceId) override {
-    return interfaceId == RouterPlugin::iid();
-  }
-  std::shared_ptr<BmcWebPlugin>
-  getInterface(const std::string &interfaceId) override {
-    if (interfaceId == RouterPlugin::iid()) {
-      return this->shared_from_this();
+class MyRouterPlugin : public RouterPlugin
+{
+  public:
+    MyRouterPlugin() {}
+    std::string registerRoutes(crow::App& app)
+    {
+        sendTestEventLog(app, app.ioContext().get_executor());
+        return "MyRouterPlugin";
     }
-    return RouterPlugin::getInterface(interfaceId);
-  }
-  void sendTestEventLog(crow::App &app, net::any_io_executor &executor) {
-    nlohmann::json logEntryArray;
-    logEntryArray.push_back({});
-    nlohmann::json &logEntryJson = logEntryArray.back();
 
-    logEntryJson["EventId"] = "TestID";
-    logEntryJson["EventType"] = "Event";
-    logEntryJson["Severity"] = "OK";
-    logEntryJson["Message"] = "Generated test event";
-    logEntryJson["MessageId"] = "OpenBMC.0.2.TestEventLog";
-    logEntryJson["MessageArgs"] = nlohmann::json::array();
-    logEntryJson["EventTimestamp"] = getCurrentTime();
-    logEntryJson["Context"] = "TestContext";
+    // Factory method
+    static std::shared_ptr<MyRouterPlugin> create()
+    {
+        return std::make_shared<MyRouterPlugin>();
+    }
 
-    nlohmann::json msg;
-    msg["@odata.type"] = "#Event.v1_4_0.Event";
-    // msg["Id"] = std::to_string(eventSeqNum);
-    msg["Name"] = "Event Log";
-    msg["Events"] = logEntryArray;
+    bool hasInterface(const std::string& interfaceId) override
+    {
+        return interfaceId == RouterPlugin::iid();
+    }
+    std::shared_ptr<BmcWebPlugin>
+        getInterface(const std::string& interfaceId) override
+    {
+        if (interfaceId == RouterPlugin::iid())
+        {
+            return this->shared_from_this();
+        }
+        return RouterPlugin::getInterface(interfaceId);
+    }
+    void sendTestEventLog(crow::App& app, net::any_io_executor executor)
+    {
+        nlohmann::json logEntryArray;
+        logEntryArray.push_back({});
+        nlohmann::json& logEntryJson = logEntryArray.back();
 
-    std::string strMsg =
-        msg.dump(2, ' ', true, nlohmann::json::error_handler_t::replace);
+        logEntryJson["EventId"] = "TestID";
+        logEntryJson["EventType"] = "Event";
+        logEntryJson["Severity"] = "OK";
+        logEntryJson["Message"] = "Generated test event";
+        logEntryJson["MessageId"] = "OpenBMC.0.2.TestEventLog";
+        logEntryJson["MessageArgs"] = nlohmann::json::array();
+        logEntryJson["EventTimestamp"] = getCurrentTime();
+        logEntryJson["Context"] = "TestContext";
 
-    EventServiceManager::getInstance(executor).sendEvent(std::move(strMsg));
-  }
-  std::string getCurrentTime() {
-    auto currentTime = std::chrono::system_clock::now();
-    std::time_t currentTime_t =
-        std::chrono::system_clock::to_time_t(currentTime);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&currentTime_t), "%Y-%m-%dT%H:%M:%S");
-    return ss.str();
-  }
+        nlohmann::json msg;
+        msg["@odata.type"] = "#Event.v1_4_0.Event";
+        // msg["Id"] = std::to_string(eventSeqNum);
+        msg["Name"] = "Event Log";
+        msg["Events"] = logEntryArray;
+
+        std::string strMsg = msg.dump(2, ' ', true,
+                                      nlohmann::json::error_handler_t::replace);
+
+        EventServiceManager::getInstance(executor).sendEvent(std::move(strMsg));
+    }
+    std::string getCurrentTime()
+    {
+        auto currentTime = std::chrono::system_clock::now();
+        std::time_t currentTime_t =
+            std::chrono::system_clock::to_time_t(currentTime);
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&currentTime_t),
+                            "%Y-%m-%dT%H:%M:%S");
+        return ss.str();
+    }
 };
 
-BMCWEB_SYMBOL_EXPORT std::shared_ptr<MyRouterPlugin> create_object() {
-  return MyRouterPlugin::create();
+BMCWEB_SYMBOL_EXPORT std::shared_ptr<MyRouterPlugin> create_object()
+{
+    return MyRouterPlugin::create();
 }
 } // namespace bmcweb
